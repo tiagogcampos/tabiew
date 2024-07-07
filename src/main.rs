d553f9eca8187562ffe6f3c3bc403521c972d50d
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2024-07-07 01:14:21 +0100

    add json supp

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use clap::Parser;
 use polars::frame::DataFrame;
 use polars::io::csv::read::{CsvParseOptions, CsvReadOptions};
-use polars::io::SerReader;
 use polars::lazy::frame::IntoLazy;
 use polars::prelude::ParquetReader;
+use polars::prelude::*;
 use polars_sql::SQLContext;
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
-use std::ffi::OsStr;
 use std::io::{self, Stderr};
 use tabiew::app::{AppResult, StatusBar, Tabular};
 use tabiew::args::{Args, InferSchema};
@@ -19,10 +18,15 @@ use tabiew::tui::Tui;
 use tabiew::utils::infer_schema_safe;
 
 fn load_parquet_file(args: &Args) -> DataFrame {
-    let mut file = std::fs::File::open(&args.file_name).unwrap();
-    let df = ParquetReader::new(&mut file).finish().unwrap();
+    let file = std::fs::File::open(&args.file_name).unwrap();
 
-    df
+    ParquetReader::new(file).finish().unwrap()
+}
+
+fn load_json_file(args: &Args) -> DataFrame {
+    let file = std::fs::File::open(&args.file_name).unwrap();
+
+    JsonReader::new(file).finish().unwrap()
 }
 
 fn load_csv_file(args: &Args) -> DataFrame {
@@ -56,27 +60,9 @@ fn main() -> AppResult<()> {
     let data_frame = match args.file_name.extension().unwrap().to_str() {
         Some("parquet") => load_parquet_file(&args),
         Some("csv") => load_csv_file(&args),
-        _ => panic!(),
+        Some("json") => load_json_file(&args),
+        _ => unimplemented!(),
     };
-
-    // Create the data frame.
-    // let data_frame = {
-    //     let mut df = CsvReadOptions::default()
-    //         .with_ignore_errors(args.ignore_errors)
-    //         .with_infer_schema_length((&args.infer_schema).into())
-    //         .with_has_header(!args.no_header)
-    //         .with_parse_options(
-    //             CsvParseOptions::default()
-    //                 .with_quote_char((args.quote_char as u8).into())
-    //                 .with_separator(args.separator as u8),
-    //         )
-    //         .try_into_reader_with_file_path(args.file_name.into())?
-    //         .finish()?;
-    //     if matches!(args.infer_schema, InferSchema::Safe) {
-    //         infer_schema_safe(&mut df);
-    //     }
-    //     df
-    // };
 
     // Setup the SQLContext
     let mut sql_context = SQLContext::new();
